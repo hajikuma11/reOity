0e053a9bd4c0ba98f1be3b3f47a39c0a3335a6ee
hajikuma11 <hajimekuma11@gmail.com>
2020-06-28 22:19:14 +0900

    オウム返しを実装

--- new/main.php
+++ new/main.php
@@ -28,39 +28,20 @@ if ($event_type == "message") {//テキスト
 
 }
 
+
+$aum = [
+  'type' => 'text',
+  'text' => $msg
+];
+
 //リプライトークン
 $token_reply = $obj_req->{"events"}[0]->{"replyToken"};
 
-/**
- * 生成したコンテンツを送信する関数
- *
- * @param array $content
- * @return void
- */
-function send ($content) {
-  global $token_access;
+$content_reply = [
+  'replyToken' => $token_reply,
+  'messages' => [$aum]
+];
 
-  $ch = curl_init('https://api.line.me/v2/bot/message/reply');
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($content));
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/json; charser=UTF-8',
-      'Authorization: Bearer ' . $token_access
-  ));
-  $result = curl_exec($ch);
-  if ($result != "{}") {
-    error_log('[@reOity@]'.$result);
-  }
-  curl_close($ch);
-}
-
-/**
- * メイン処理を行う関数
- */
-function main () {
-  send($content);
-}
-
-main();
+//送信
+require('send.php');
+send($content,$token_access);
