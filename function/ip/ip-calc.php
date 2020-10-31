28f45cc34106a0a88c5c063b96fe5ca2328add82
hajikuma11 <hajimekuma11@gmail.com>
2020-11-01 01:12:40 +0900

    ネットワークアドレス計算できるようになった

--- function/ip/ip-calc.php
+++ function/ip/ip-calc.php
@@ -1,11 +1,25 @@
 <?php
+//テスト用メッセージ
+$msg = '192.168.1.10/28';
+preg_match('/([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})/',$msg,$ip);
 
-switch (true) {
-  case :// IPアドレス+サブネットマスクを検出
+if (preg_match('/([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})\/([0-9]{1,2})/',$msg,$msg_arr)) {
+  $mask = $msg_arr[5];
+} else {
+  require_once(__DIR__.'/mask-q.php');
+}
 
-  break;
+$subnetmask = [];
+$subnetmask = array_pad($subnetmask, $mask, 1);
+$subnetmask = array_pad($subnetmask, 32, 0);
+$subnetmask_arr = str_split(implode("",$subnetmask), 8);
 
-  case :// IPアドレスのみを検出
+$ip_dec_arr = [str_pad(decbin($ip[1]), 8, '0', STR_PAD_LEFT),str_pad(decbin($ip[2]), 8, '0', STR_PAD_LEFT),str_pad(decbin($ip[3]), 8, '0', STR_PAD_LEFT),str_pad(decbin($ip[4]), 8, '0', STR_PAD_LEFT)];
 
-  break;
+for ($i=0;$i<4;$i++) {
+  $res = $subnetmask_arr[$i] & $ip_dec_arr[$i];
+  echo bindec($res);
+  if ($i < 3) {
+    echo '.';
+  }
 }
