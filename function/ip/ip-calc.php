ddc6cb214eaabda8cc06388bcaa1d033e339107d
hajikuma11 <hajimekuma11@gmail.com>
2020-10-31 22:14:40 +0900

    名称の変更

---
+++ function/ip/ip-calc.php
@@ -0,0 +1,11 @@
+<?php
+
+switch (true) {
+  case :// IPアドレス+サブネットマスクを検出
+
+  break;
+
+  case :// IPアドレスのみを検出
+
+  break;
+}
--- main.php
+++ main.php
@@ -58,8 +58,8 @@ switch (true) {
     require_once(__DIR__.'/function/janken/janken.php');
   break;
 
-  case preg_match('/([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})/'.$msg):
-    require_once(__DIR__.'/function/ip/ip.php');
+  case preg_match('/([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})/',$msg,$ip):
+    require_once(__DIR__.'/function/ip-calc/ip-calc.php');
   break;
 
   default:
